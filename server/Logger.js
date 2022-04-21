e6aefdd9fa2d25148b98cc3b0611e5337166c649
Doğukan Göker <dogukangkr1234@gmail.com>
2022-04-21 17:46:35 +0300

    logging improvements

--- server/Logger.js
+++ server/Logger.js
@@ -2,26 +2,62 @@ import chalk from "chalk";
 
 export default class Logger {
   static log(...args) {
-    console.log(chalk.green(...args));
+    console.log(
+      `(${
+        new Date().toLocaleDateString() +
+        " - " +
+        new Date().toLocaleTimeString()
+      }): ` + chalk.green(...args)
+    );
   }
 
   static error(...args) {
-    console.log(chalk.red(...args));
+    console.log(
+      `(${
+        new Date().toLocaleDateString() +
+        " - " +
+        new Date().toLocaleTimeString()
+      }): ` + chalk.red(...args)
+    );
   }
 
   static warn(...args) {
-    console.log(chalk.yellow(...args));
+    console.log(
+      `(${
+        new Date().toLocaleDateString() +
+        " - " +
+        new Date().toLocaleTimeString()
+      }): ` + chalk.yellow(...args)
+    );
   }
 
   static info(...args) {
-    console.log(chalk.blue(...args));
+    console.log(
+      `(${
+        new Date().toLocaleDateString() +
+        " - " +
+        new Date().toLocaleTimeString()
+      }): ` + chalk.blue(...args)
+    );
   }
 
   static debug(...args) {
-    console.log(chalk.gray(...args));
+    console.log(
+      `(${
+        new Date().toLocaleDateString() +
+        " - " +
+        new Date().toLocaleTimeString()
+      }): ` + chalk.gray(...args)
+    );
   }
 
   static trace(...args) {
-    console.log(chalk.gray(...args));
+    console.log(
+      `(${
+        new Date().toLocaleDateString() +
+        " - " +
+        new Date().toLocaleTimeString()
+      }): ` + chalk.gray(...args)
+    );
   }
 }
